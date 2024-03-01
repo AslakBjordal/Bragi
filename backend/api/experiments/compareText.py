4ab12d959174eec5e2d26ebc02369fd11a7d578a
snorre <snorre6@hotmail.com>
2024-03-01 15:10:24 +0100

    split mp3 into chunks for live sub and timestamp

--- backend/api/experiments/compareText.py
+++ backend/api/experiments/compareText.py
@@ -34,18 +34,24 @@ def calculate_similarity(file1_path, file2_path):
 
 
 if __name__ == "__main__":
-    file_fasit  = 'text/fasit.txt'
-    file_tiny   = 'text/test_tiny.txt'
-    file_base   = 'text/test_base.txt'
-    file_small  = 'text/test_small.txt'
-    file_medium = 'text/test_medium.txt'
-    file_large  = 'text/test_large.txt'
+    file_fasit      = 'text/fasit.txt'
+    file_tiny       = 'text/test_tiny.txt'
+    file_base       = 'text/test_base.txt'
+    file_small      = 'text/test_small.txt'
+    file_medium     = 'text/test_medium.txt'
+    file_large      = 'text/test_large.txt'
+    file_large_v2   = 'text/test_large-v2.txt'
+    file_large_v3   = 'text/test_large-v3.txt'
+    file_small_chunk = 'text/compiled_transcriptions.txt'
 
     similarity_score_tiny   = calculate_similarity(file_fasit, file_tiny)
     similarity_score_base   = calculate_similarity(file_fasit, file_base)
     similarity_score_small  = calculate_similarity(file_fasit, file_small)
     similarity_score_medium = calculate_similarity(file_fasit, file_medium)
     similarity_score_large  = calculate_similarity(file_fasit, file_large)
+    similarity_score_large_v2  = calculate_similarity(file_fasit, file_large_v2)
+    similarity_score_large_v3  = calculate_similarity(file_fasit, file_large_v3)
+    similarity_score_small_chunk = calculate_similarity(file_fasit, file_small_chunk)
     
     # write the result to a text file named result.txt
     with open('result/result.txt', 'w') as file:
@@ -54,6 +60,9 @@ if __name__ == "__main__":
         file.write(f"Similarity score between fasit and small: {similarity_score_small}\n")
         file.write(f"Similarity score between fasit and medium: {similarity_score_medium}\n")
         file.write(f"Similarity score between fasit and large: {similarity_score_large}\n")
+        file.write(f"Similarity score between fasit and large-v2: {similarity_score_large_v2}\n")
+        file.write(f"Similarity score between fasit and large-v3: {similarity_score_large_v3}\n")
+        file.write(f"Similarity score between fasit and small_chunk: {similarity_score_small_chunk}\n")
 
 
 
