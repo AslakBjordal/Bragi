9153ac3347ab6fdfccff77831cec1bef887c212a
AslakBjordal <Abj084@post.uit.no>
2024-03-07 10:57:23 +0100

    Started with frontend for Bragi

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import "carbon-components-svelte/css/all.css";
+  import { TextInput, Button } from "carbon-components-svelte";
+</script>
+
+<body>
+  <div class="input-container">
+    <div class="inputbox">
+      <TextInput placeholder="Insert Video link" />
+      <Button>Caption Video</Button>
+    </div>
+  </div>
+</body>
+
+<style>
+  .inputbox {
+    width: 400px;
+    display: flex;
+  }
+  .input-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+</style>
