e1c5a5ab1ba8dd831e74b60d8199f068cacb2d4f
AslakBjordal <Abj084@post.uit.no>
2024-04-25 13:29:26 +0200

    Added scripting that allows the extension to get the video url and the current progress in the video.

--- bragi-chrome/src/routes/+page.svelte
+++ bragi-chrome/src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let url = '';
+	let runCaptions = false;
+	let captions = '';
+	let timerValue = '';
+	async function getUrl() {
+		const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+		url = tab.url ?? '';
+		if (url.includes('www.youtube.com/watch?v')) {
+			chrome.scripting
+				.executeScript({
+					target: { tabId: tab.id ?? 0 },
+					func: getCurrentTime
+				})
+				.then((results) => {
+					for (const { result } of results) {
+						timerValue = result?.toString() ?? 'no video found';
+					}
+				});
+		}
+	}
+	function activateCaptions() {
+		runCaptions = !runCaptions;
+	}
+	function getCurrentTime() {
+		return document.querySelector('video')?.currentTime;
+	}
+
+	if (runCaptions) {
+		captions = 'UseAPI and get captions';
+	}
+</script>
+
+<button on:click={getUrl}>Reveal url</button>
+<button on:click={activateCaptions}>Toggle captions</button>
+{#if url != ''}
+	<div>
+		Current url is {url}
+		{#if timerValue}
+			<div>Current progress in video {timerValue}</div>
+		{/if}
+	</div>
+{/if}
